729d99aa00cb259d138ad38a2d9c949595ba78c4
CarAndAsh <zeixes@gmail.com>
2026-01-13 00:18:31 +0300

    add Jinja2Templates into config

--- app/core/config.py
+++ app/core/config.py
@@ -2,20 +2,26 @@ from pathlib import Path
 
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from fastapi.templating import Jinja2Templates
 
-APP_DIR = Path(__name__).resolve().parent.parent
+APP_DIR = Path(__name__).resolve().parent
+
 
 class RunConfig(BaseModel):
     host: str
     port: int
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(APP_DIR / '.env',),
+        env_file=(APP_DIR.parent / '.env',),
         env_nested_delimiter='-',
         case_sensitive=False,
+        extra = 'allow'
     )
     run: RunConfig
+    templates: Jinja2Templates = Jinja2Templates(APP_DIR / 'templates')
+
 
 settings = Settings()
 
