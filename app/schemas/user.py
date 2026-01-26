0549401b8f2a770622cdb34e60840af379f16e3d
CarAndAsh <zeixes@gmail.com>
2026-01-26 17:14:11 +0300

    modify UserReadSchema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,18 +2,21 @@ from pydantic import BaseModel
 
 
 class UserSchema(BaseModel):
+    telegram_id: int
     is_bot: bool
-    first_name: str
-    last_name: str
+    first_name: str | None
+    last_name: str | None
     username: str
     language_code: str
 
+
 class ReadUserSchema(UserSchema):
-    id:int
+    id: int
+
 
 class CreateUserSchema(UserSchema):
     pass
 
+
 class DeleteUserSchema(ReadUserSchema):
     pass
-
