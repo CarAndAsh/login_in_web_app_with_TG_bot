112e4264faf673e5874e80dbcfd0867c709fca6d
CarAndAsh <zeixes@gmail.com>
2026-01-26 17:20:56 +0300

    create lifespan for app

--- app/create_app.py
+++ app/create_app.py
@@ -1,17 +1,30 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from uvicorn import Server, Config
 
+from app.api import api_router
 from app.core.app_log_cofig import app_logger
 from app.core.app_config import settings
+from app.models import db_helper
 from app.views import views_router
 
 app_logger.name = __file__
 
-app = FastAPI()
-app.include_router(views_router)
+
 
 
 async def start_web_app():
+
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        yield
+        await db_helper.dispose()
+
+    app = FastAPI(lifespan=lifespan)
+    app.include_router(views_router)
+    app.include_router(api_router)
+
     app_logger.debug('Приложение запущено')
     server_config = Config(app, host=settings.run.host, port=settings.run.port)
     app_server = Server(server_config)
