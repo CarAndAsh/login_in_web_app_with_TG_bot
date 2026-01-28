92d712120a1de9d71b19786137945657287d5c4e
CarAndAsh <zeixes@gmail.com>
2026-01-28 16:46:02 +0300

    update route for user_page template by telegram id

--- app/views/user_page.py
+++ app/views/user_page.py
@@ -1,10 +1,21 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Request
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.app_config import settings
+from app.crud.users import get_user_by_tg_id
+from app.models import db_helper
 
-router = APIRouter(include_in_schema=False, prefix='/user_page')
+router = APIRouter(include_in_schema=False, tags=['For_templates',])
 
-@router.get('/', name='user_page')
-def user_page(req: Request):
-    context = {}
+@router.get('/user_page/{tg_id:int}', name='user_page')
+async def user_page_by_tg_id(
+        req: Request,
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        tg_id: int
+        ):
+    user = await get_user_by_tg_id(session, tg_id)
+    context = user.to_dict() or {}
     return settings.templates.TemplateResponse(req, 'user_page.html', context)
