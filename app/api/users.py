a04b4db67a1fd238b6c89cec86de48680ba7c96a
CarAndAsh <zeixes@gmail.com>
2026-01-28 16:56:36 +0300

    create route for getting user data by telegram id

--- app/api/users.py
+++ app/api/users.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from fastapi import Request, APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.crud.users import get_user_by_tg_id
 from app.models import db_helper
 from app.schemas.user import ReadUserSchema, UserSchema
 from app.crud import users_crud
@@ -23,3 +24,12 @@ async def add_user_data_by_tg_bot(req: Request, session: Annotated[AsyncSession,
 @router.get('/', name='users', response_model=list[UserSchema])
 async def get_all_users(session: Annotated[AsyncSession, Depends(db_helper.session_getter)]):
     return await users_crud.get_all_users(session)
+
+
+@router.get('/{tg_id:int}', name='user_by_telegram_id', response_model=ReadUserSchema | None)
+async def user_page_by_tg_id(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        tg_id: int
+):
+    user = await get_user_by_tg_id(session, tg_id)
+    return user
