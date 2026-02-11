4f0d3505a6c7b417de6e46b2d378d35ad38e73ee
CarAndAsh <zeixes@gmail.com>
2026-02-11 15:50:24 +0300

    add crud operations for user

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,19 +1,20 @@
-from typing import Sequence
+from typing import Sequence, Any
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import User
+from app.schemas.user import UpdateUserSchema, PartialUpdateUserSchema
 
 
-async def create_user(session: AsyncSession, user_create: dict) -> User:
+async def create_user(session: AsyncSession, user_create: dict[str, Any]) -> User:
     user = User(**user_create)
     session.add(user)
     await session.commit()
     return user
 
 
-async def get_user(session: AsyncSession, user_id) -> User | None:
+async def get_user(session: AsyncSession, user_id: int) -> User | None:
     return await session.get(User, user_id)
 
 
@@ -27,9 +28,19 @@ async def get_all_users(session: AsyncSession) -> Sequence[User]:
     return query.all()
 
 
-async def update_user(session: AsyncSession):
-    pass
+async def update_user(
+        session: AsyncSession,
+        user_by_tg_id: User,
+        user_update: UpdateUserSchema | PartialUpdateUserSchema,
+        partial:bool = False) -> User:
+    for key, value in user_update.model_dump(exclude_unset=partial).items():
+        setattr(user_by_tg_id, key, value)
+    await session.commit()
+    return user_by_tg_id
 
 
-async def delete_user(session: AsyncSession):
-    pass
+
+async def delete_user(session: AsyncSession, user_by_tg_id: User) -> None:
+    await session.delete(user_by_tg_id)
+    print('deleting user')
+    await session.commit()
