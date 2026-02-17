234710dc69c852683864b89ee2a38c12b9451d5b
CarAndAsh <zeixes@gmail.com>
2026-02-17 17:33:32 +0300

    delete users routes except get_all_users

--- app/api/users.py
+++ app/api/users.py
@@ -6,7 +6,7 @@ from fastapi import Request, APIRouter, Depends, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import db_helper
-from app.schemas.user import UserSchema, UpdateUserSchema, PartialUpdateUserSchema, CreateUserSchema
+from app.schemas.user import UserSchema, PartialUpdateUserSchema, CreateUserSchema
 from app.crud import users_crud, get_user_by_tg_id
 
 
@@ -39,40 +39,3 @@ async def get_all_users(
     log.info('you get all users list')
     users_list = await users_crud.get_all_users(session)
     return list(map(lambda user: UserSchema.model_validate(user), users_list))
-
-
-@router.get('/{tg_id:int}/', name='user_by_telegram_id')
-async def user_page_by_tg_id(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-        tg_id: int
-) -> UserSchema:
-    user = await get_user_by_tg_id(session, tg_id)
-    return UserSchema.model_validate(user)
-
-
-@router.patch('/user/', name='update_some_user_data_by_telegram_id')
-async def part_update_user_by_tg_id(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-        user_data: Annotated[UserSchema, Depends(get_user_by_tg_id)],
-        user_update: PartialUpdateUserSchema
-) -> UserSchema:
-    user = await users_crud.update_user(session, user_data, user_update, partial=True)
-    return UserSchema.model_validate(user)
-
-
-@router.put('/user/', name='update_user_data_by_telegram_id')
-async def update_user_by_tg_id(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-        user_data: Annotated[UserSchema, Depends(get_user_by_tg_id)],
-        user_update: UpdateUserSchema
-) -> UserSchema:
-    user = await users_crud.update_user(session, user_data, user_update)
-    return UserSchema.model_validate(user)
-
-
-@router.delete('/user/', name='delete_user_by_telegram_id')
-async def delete_user_by_tg_id(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-        user_data: Annotated[UserSchema, Depends(get_user_by_tg_id)]
-) -> None:
-    await users_crud.delete_user(session, user_data)
