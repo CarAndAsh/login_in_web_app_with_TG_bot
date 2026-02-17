390220da24f2d068bd76e6eec199f290d98cf064
CarAndAsh <zeixes@gmail.com>
2026-02-17 17:34:31 +0300

    add fastapi-users routers

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,8 @@ __all__ = ('api_router',)
 from fastapi import APIRouter
 
 from .users import router
+from .fastapi_users_routes import fastapi_users_router
 
 api_router = APIRouter()
 api_router.include_router(router)
+api_router.include_router(fastapi_users_router)
---
+++ app/api/fastapi_users_routes.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from fastapi_users import FastAPIUsers
+
+from app.api.dependencies.authentication import auth_backend, get_user_manager
+from app.models import User
+from app.schemas.user import UserSchema, PartialUpdateUserSchema, CreateUserSchema
+
+fastapi_users = FastAPIUsers[User, int](get_user_manager,[auth_backend])
+
+fastapi_users_router = APIRouter(tags=['FastAPI-Users'])
+
+# /login, /logout
+fastapi_users_router.include_router(fastapi_users.get_auth_router(auth_backend))
+# /register
+fastapi_users_router.include_router(fastapi_users.get_register_router(UserSchema, CreateUserSchema))
+# /request-verify-token, /verify
+fastapi_users_router.include_router(fastapi_users.get_verify_router(UserSchema))
+# /forgot-password, /reset-password
+fastapi_users_router.include_router(fastapi_users.get_reset_password_router())
+# PATCH /me, /id
+fastapi_users_router.include_router(fastapi_users.get_users_router(UserSchema, PartialUpdateUserSchema))
+# PATCH and DELETE users use user.get(id)
