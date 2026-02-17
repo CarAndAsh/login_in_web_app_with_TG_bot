fcf6cf10d53a81090c39f944bc8847634dc80dbc
CarAndAsh <zeixes@gmail.com>
2026-02-17 17:03:50 +0300

    change schemas inheritance

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,38 +1,28 @@
-from pydantic import ConfigDict, BaseModel
+from fastapi_users.schemas import BaseUser, BaseUserCreate, BaseUserUpdate
 
+# TODO something for hide 'id' field
 
-class BaseUserSchema(BaseModel):
+class BaseUserSchema(BaseUser[int]):
     telegram_id: int | None
     is_bot: bool
     first_name: str | None
     last_name: str | None
     username: str
     language_code: str
-    is_active: bool = True
-    is_superuser: bool = False
-    is_verified: bool = False
-
-    model_config = ConfigDict(from_attributes=True)
 
 
 class UserSchema(BaseUserSchema):
-    id: int
-
-
-class UpdateUserSchema(BaseUserSchema):
     pass
 
 
-class PartialUpdateUserSchema(BaseModel):
+class PartialUpdateUserSchema(BaseUserSchema, BaseUserUpdate):
     telegram_id: int | None = None
     is_bot: bool | None = None
     first_name: str | None = None
     last_name: str | None = None
     username: str | None = None
     language_code: str | None = None
-    is_active: bool = True
-    is_verified: bool = False
 
 
-class CreateUserSchema(BaseUserSchema):
+class CreateUserSchema(BaseUserSchema, BaseUserCreate):
     pass
