258dc4e8e2bb754903b464cb309b133cb4847e53
CarAndAsh <zeixes@gmail.com>
2026-03-12 18:00:18 +0300

    return TYPE_CHECKING condition for auth dependencies

--- app/api/dependencies/authentication.py
+++ app/api/dependencies/authentication.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING, Annotated
+
 from fastapi.params import Depends
 from fastapi_users.authentication import AuthenticationBackend, JWTStrategy
 
@@ -6,7 +8,8 @@ from app.core.authentication.transport import bearer_transport
 from app.core.authentication.user_manager import UserManager
 from app.crud.dependencies import get_users_db
 
-from fastapi_users.db import SQLAlchemyUserDatabase
+if TYPE_CHECKING:
+    from fastapi_users.db import SQLAlchemyUserDatabase
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(settings.jwt_strategy.secret, settings.jwt_strategy.lifetime_sec)
@@ -15,6 +18,6 @@ auth_backend = AuthenticationBackend(
     name='jwt', transport=bearer_transport, get_strategy=get_jwt_strategy
 )
 
-async def get_user_manager(users_db: SQLAlchemyUserDatabase = Depends(get_users_db)):
+async def get_user_manager(users_db: Annotated['SQLAlchemyUserDatabase', Depends(get_users_db)]):
     yield UserManager(users_db)
 
