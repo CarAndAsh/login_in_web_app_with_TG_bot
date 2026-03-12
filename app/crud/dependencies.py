f3978fa7c754ae490b4a1c724f6125a43995e499
CarAndAsh <zeixes@gmail.com>
2026-03-12 17:56:28 +0300

    return TYPE_CHECKING condition

--- app/crud/dependencies.py
+++ app/crud/dependencies.py
@@ -1,15 +1,16 @@
-from typing import Annotated, AsyncGenerator
+from typing import Annotated, AsyncGenerator, TYPE_CHECKING
 
 from fastapi.params import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import User, db_helper
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 async def get_user_by_tg_id(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        session: Annotated['AsyncSession', Depends(db_helper.session_getter)],
         user_tg_id: int) -> User | None:
     query = await session.execute(select(User).filter(User.telegram_id == user_tg_id))
     user = query.scalar()
@@ -17,6 +18,6 @@ async def get_user_by_tg_id(
 
 
 async def get_users_db(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
-) -> AsyncGenerator[AsyncSession, None]:
+        session: Annotated['AsyncSession', Depends(db_helper.session_getter)]
+) -> AsyncGenerator['AsyncSession', None]:
     yield SQLAlchemyUserDatabase(session, User)
