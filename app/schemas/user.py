dffb9cc0419eeb37c8235fb65fa07e24cbecc336
CarAndAsh <zeixes@gmail.com>
2026-03-12 17:48:44 +0300

    change user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,28 +1,27 @@
 from fastapi_users.schemas import BaseUser, BaseUserCreate, BaseUserUpdate
+from pydantic import EmailStr, BaseModel, ConfigDict
+
 
 # TODO something for hide 'id' field
 
-class BaseUserSchema(BaseUser[int]):
+class TelegramUserSchema(BaseModel):
     telegram_id: int | None
-    is_bot: bool
+    is_bot: bool = False
     first_name: str | None
     last_name: str | None
     username: str
     language_code: str
 
 
-class UserSchema(BaseUserSchema):
-    pass
+class UserSchema(BaseUser[int], TelegramUserSchema):
+    email: EmailStr | None = None
 
 
-class PartialUpdateUserSchema(BaseUserSchema, BaseUserUpdate):
-    telegram_id: int | None = None
+class PartialUpdateUserSchema(UserSchema, BaseUserUpdate, TelegramUserSchema):
     is_bot: bool | None = None
-    first_name: str | None = None
-    last_name: str | None = None
     username: str | None = None
     language_code: str | None = None
 
 
-class CreateUserSchema(BaseUserSchema, BaseUserCreate):
-    pass
+class CreateUserSchema(UserSchema, BaseUserCreate, TelegramUserSchema):
+    email: EmailStr | None = None
