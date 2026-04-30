d0548553ba89112eea9b593fca301eaf58d009ce
CarAndAsh <zeixes@gmail.com>
2026-04-30 15:53:52 +0300

    create schemas for forms

---
+++ app/schemas/forms.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from starlette_wtf import StarletteForm
+from wtforms import StringField, SubmitField,PasswordField, EmailField
+
+
+class RegisterForm(StarletteForm):
+    first_name = StringField('Имя')
+    last_name = StringField('Фамилия')
+    username = StringField('Никнейм')
+    email = EmailField('E-mail')
+    password = PasswordField('Пароль')
+    confirm_password = PasswordField('Подтверждение')
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(StarletteForm):
+    email = EmailField('E-mail')
+    password = PasswordField('Пароль')
+    submit = SubmitField('Войти')
