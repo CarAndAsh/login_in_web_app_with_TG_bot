8dc2ea67ed254a12833e54ab3170ba454ac800d5
CarAndAsh <zeixes@gmail.com>
2026-04-30 15:51:16 +0300

    include router for forms

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,7 +2,9 @@ __all__ = ('views_router',)
 
 from fastapi import APIRouter
 
-from app.views.user_page import router
+from app.views.user_page import router as user_page_router
+from app.views.forms import router as forms_router
 
 views_router = APIRouter()
-views_router.include_router(router)
+views_router.include_router(user_page_router)
+views_router.include_router(forms_router)
