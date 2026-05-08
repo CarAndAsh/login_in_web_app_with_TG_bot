502fd8de31d0812ee56a36259955bdf46cf558cf
CarAndAsh <zeixes@gmail.com>
2026-05-08 17:16:23 +0300

    create UserForm

--- app/schemas/forms.py
+++ app/schemas/forms.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from starlette_wtf import StarletteForm
-from wtforms import StringField, SubmitField,PasswordField, EmailField
+from wtforms import (StringField, SubmitField, PasswordField, EmailField, BooleanField)
 
 
 class RegisterForm(StarletteForm):
@@ -17,6 +17,18 @@ class LoginForm(StarletteForm):
     email = EmailField('E-mail')
     password = PasswordField('Пароль')
     submit = SubmitField('Войти')
+
+
+class UserForm(StarletteForm):
+    first_name = StringField('Имя')
+    last_name = StringField('Фамилия')
+    telegram_id = StringField('телеграм ID')
+    email = EmailField('E-mail')
+    username = StringField('Никнейм')
+    password = PasswordField('Пароль')
+    confirm_password = PasswordField('Подтверждение')
+    is_verified = BooleanField('Подтвержден')
+    submit = SubmitField('Сохранить')
 
 
 class LoginDataForm(BaseModel):
