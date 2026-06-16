96e88cd6e06ff53efec6392a4b2f7fa0902126ae
CarAndAsh <zeixes@gmail.com>
2026-06-16 16:04:06 +0300

    add tag for views router

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -6,7 +6,7 @@ from app.views.user_page import router as user_page_router
 from app.views.forms import router as forms_router
 from app.views.other import router as other_router
 
-views_router = APIRouter()
+views_router = APIRouter(tags=['Views'])
 views_router.include_router(user_page_router)
 views_router.include_router(forms_router)
 views_router.include_router(other_router)
