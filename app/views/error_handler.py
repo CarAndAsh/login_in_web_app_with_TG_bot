f3f5a7f9f0c2aa52dc9de64d8c515822b2e74d4a
CarAndAsh <zeixes@gmail.com>
2026-06-29 20:12:35 +0300

    add UserAlreadyExist handle in error_handler and status_codes

--- app/views/error_handler.py
+++ app/views/error_handler.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response, Request, status, HTTPException
+from fastapi_users.exceptions import UserAlreadyExists
 
 from app.core.app_config import settings
 
@@ -7,10 +8,18 @@ def register_errors_handlers(app: FastAPI):
     @app.exception_handler(AttributeError)
     def handle_allow_error(req: Request, exc: AttributeError) -> Response:
         exc_context = {
-        'detail': 'Incorrect login or password',
+        'detail': 'Неверный логин или пароль',
         'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR
         }
-        return settings.templates.TemplateResponse(req, 'error.html', exc_context)
+        return settings.templates.TemplateResponse(req, 'error.html', exc_context, status_code=500)
+
+    @app.exception_handler(UserAlreadyExists)
+    def handle_allow_error(req: Request, exc: UserAlreadyExists) -> Response:
+        exc_context = {
+        'detail': 'Пользователь с такими данными уже существует.',
+        'status_code': status.HTTP_400_BAD_REQUEST
+        }
+        return settings.templates.TemplateResponse(req, 'error.html', exc_context, status_code=400)
 
     @app.exception_handler(status.HTTP_401_UNAUTHORIZED)
     def handle_unauthorized_error(req: Request, exc: HTTPException) -> Response:
@@ -18,4 +27,4 @@ def register_errors_handlers(app: FastAPI):
         'detail': exc.detail,
         'status_code': exc.status_code
         }
-        return settings.templates.TemplateResponse(req, 'error.html', exc_context)
+        return settings.templates.TemplateResponse(req, 'error.html', exc_context, status_code=401)
