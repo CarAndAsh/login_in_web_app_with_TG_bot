a467a4b0b6717e0f20f054f055e7c51b4ee0e197
CarAndAsh <zeixes@gmail.com>
2026-07-08 15:39:53 +0300

    add DeleteUserMessageMiddleware

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,7 +2,9 @@ from aiogram import Router
 
 from bot.handlers.other import other_router
 from bot.handlers.user import user_router
+from bot.middlewares.outer import DeleteUserMessageMiddleware
 
 router: Router = Router()
 router.include_routers(user_router, other_router)
 
+router.message.outer_middleware(DeleteUserMessageMiddleware())
---
+++ bot/middlewares/outer.py
@@ -0,0 +1,29 @@
+from typing import Any, Callable, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+
+
+class DeleteUserMessageMiddleware(BaseMiddleware):
+    id_dict:dict = {}
+
+
+    async def __delete_prev_chat_message__(self, event, owner_id) -> None:
+        if del_msg_id := self.id_dict.get(owner_id):
+            await event.chat.delete_message(message_id=del_msg_id)
+
+
+    async def  __call__(
+            self,
+            handler: Callable[[Message, dict[str, Any]],Awaitable[Any]],
+            event: Message,
+            data: dict[str, Any],
+) -> Message:
+
+        user_id: int = event.from_user.id
+        await self.__delete_prev_chat_message__(event, event.bot.id)
+        self.id_dict[user_id] = event.message_id
+        out_msg: Message = await handler(event, data)
+        await self.__delete_prev_chat_message__(event, user_id)
+        self.id_dict[out_msg.from_user.id] = out_msg.message_id
+        return out_msg
