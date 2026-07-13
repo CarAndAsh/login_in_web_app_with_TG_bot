b06f251dee2579439ed5d031cf5b10b0f1f00c48
CarAndAsh <zeixes@gmail.com>
2026-07-13 18:13:01 +0300

    change echo route to delete any not valid message on any state of FSM

--- bot/handlers/other.py
+++ bot/handlers/other.py
@@ -2,6 +2,7 @@ from logging import getLogger
 
 from aiogram import Router
 from aiogram.filters import Command
+from aiogram.fsm.state import any_state
 from aiogram.types import Message
 
 from bot.keyboards.keyboards import reply_keyboard
@@ -17,8 +18,8 @@ async def info(msg: Message) -> Message:
     return await msg.answer(BOT_INFO['description']+'\nДля входа на сайт нажмите кнопку ниже 👇', reply_markup=reply_keyboard)
 
 
-@other_router.message()
-async def echo(msg: Message) -> Message:
+@other_router.message(any_state)
+async def delete_msg(msg: Message):
     if msg.text:
-        log.info(f'Поступило сообщение: {msg.text}')
-        return await msg.answer(msg.text)
+        log.info(f'Поступило сообщение: {msg.text}, и было удалено')
+        return await msg.delete()
