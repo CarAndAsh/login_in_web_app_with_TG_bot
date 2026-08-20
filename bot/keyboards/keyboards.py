591fd1a8d130bdef7adf216e9ac8a89de6310278
CarAndAsh <zeixes@gmail.com>
2026-08-20 16:12:00 +0300

    add function for inline_keyboard with url as attribute

--- bot/keyboards/keyboards.py
+++ bot/keyboards/keyboards.py
@@ -4,3 +4,7 @@ from bot.lexicon.lexicon_ru import BOT_BTN
 
 get_info_btn = InlineKeyboardButton(text=BOT_BTN['get_info'], callback_data='check_user')
 reply_keyboard = InlineKeyboardMarkup(inline_keyboard=[[get_info_btn],])
+
+def link_keyboard(url: str):
+    url_btn = InlineKeyboardButton(text=BOT_BTN['profile'], url=url)
+    return InlineKeyboardMarkup(inline_keyboard=[[url_btn],])
