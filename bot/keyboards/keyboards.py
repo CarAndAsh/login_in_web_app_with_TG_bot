7f57380e2d789a0884bb5b3a69f7665576996cf4
CarAndAsh <zeixes@gmail.com>
2026-09-02 18:12:31 +0300

    add keyboard for confirm/deny update account data

--- bot/keyboards/keyboards.py
+++ bot/keyboards/keyboards.py
@@ -5,6 +5,15 @@ from bot.lexicon.lexicon_ru import BOT_BTN
 get_info_btn = InlineKeyboardButton(text=BOT_BTN['get_info'], callback_data='check_user')
 reply_keyboard = InlineKeyboardMarkup(inline_keyboard=[[get_info_btn],])
 
+confirm_account_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+       [
+           InlineKeyboardButton(text='Да', callback_data='confirm_update_account'),
+            InlineKeyboardButton(text='Нет', callback_data='deny_update_account'),
+       ]
+    ]
+)
+
 def link_keyboard(url: str):
     url_btn = InlineKeyboardButton(text=BOT_BTN['profile'], url=url)
     return InlineKeyboardMarkup(inline_keyboard=[[url_btn],])
