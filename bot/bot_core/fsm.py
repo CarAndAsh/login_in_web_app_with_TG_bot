a1fa935522dc3bdff6d5f7ce56a7f06d5bb436bd
CarAndAsh <zeixes@gmail.com>
2026-09-02 18:10:07 +0300

    update FSM states

--- bot/bot_core/fsm.py
+++ bot/bot_core/fsm.py
@@ -3,7 +3,6 @@ from aiogram.fsm.state import StatesGroup, State
 class FSMAuthUser(StatesGroup):
     check_user = State()
     email_fill = State()
-    password_fill = State()
-    confirm_pwd_fill = State()
-    login = State()
-    register = State()
+    login_password_fill = State()
+    register_password_fill = State()
+    update_password_fill = State()
